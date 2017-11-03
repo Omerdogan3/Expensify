6c99ee04baeb522ecfe61afc9e37e99c07fb772b
Omer Dogan <omerdogan3@gmail.com>
2017-11-03 12:04:23 +0300

    initial

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+
+
+ReactDOM.render(<p>Heyyo</p>,document.getElementById('app'));
