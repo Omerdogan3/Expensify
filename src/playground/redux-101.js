00bc3229337f9e890f4b486f403f47c705e7b914
Omer Dogan <omerdogan3@gmail.com>
2017-11-05 14:43:54 +0300

    installing redux

---
+++ src/playground/redux-101.js
@@ -0,0 +1,35 @@
+import { createStore } from 'redux';
+
+const store = createStore((state = {count: 0}, action) => {
+    switch(action.type){
+        case 'INCREMENT':
+            return{
+                count:state.count+1
+            };
+        case 'DECREMENT':
+            return{
+                count:state.count-1
+            };
+        case 'RESET':
+            return{
+                count: 0
+            }
+        
+        default:
+            return state;
+    }
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+store.dispatch({
+    type: 'RESET'
+});
+
+console.log(store.getState());
