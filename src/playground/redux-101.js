5d85822afd050538c65a077f809c3ab73a1a80eb
Omer Dogan <omerdogan3@gmail.com>
2017-11-07 18:33:36 +0300

    redux CreateStore

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,16 +3,22 @@ import { createStore } from 'redux';
 const store = createStore((state = {count: 0}, action) => {
     switch(action.type){
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy:1; 
             return{
-                count:state.count+1
+                count:state.count+incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy:1;
             return{
-                count:state.count-1
+                count:state.count-decrementBy
             };
         case 'RESET':
             return{
                 count: 0
+            }
+        case 'SET':
+            return{
+                count: action.count
             }
         
         default:
@@ -20,16 +26,33 @@ const store = createStore((state = {count: 0}, action) => {
     }
 });
 
-store.dispatch({
-    type: 'INCREMENT'
+
+const unsubscribe = store.subscribe(()=>{
+    console.log(store.getState());
 });
 
 store.dispatch({
-    type: 'DECREMENT'
+    type: 'INCREMENT',
+    incrementBy:5
+});
+
+// unsubscribe();
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy:10
 });
 
 store.dispatch({
     type: 'RESET'
 });
 
-console.log(store.getState());
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy:10
+});
+
+store.dispatch({
+    type: 'SET',
+    count:101
+});
