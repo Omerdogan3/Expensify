28028dda4abc351053d4532a328fbd2fa464f073
Omer Dogan <omerdogan3@gmail.com>
2017-11-09 19:09:29 +0300

    destructing

--- src/playground/destructing.js
+++ src/playground/destructing.js
@@ -1,3 +1,4 @@
+//Object destructuring
 const person = {
     name:'Andrew',
     age:26,
@@ -6,12 +7,9 @@ const person = {
         temp:92
     }
 };
-
 //const {name, age} = person;
 //const name = person.name;
 //const age = person.age;
-
-
 const book = {
     title: 'Ego is the enemy',
     author: 'Ryan Holiday',
@@ -19,6 +17,12 @@ const book = {
         name:'Penguin'
     }
 };
-
 const {title, author} = book;
 const {name = 'Self-published'} = book.publisher;
+
+console.log(`Book's title: ${title}, author: ${author}, Publisher Name: ${name}`);
+
+//Array Destructuring
+const address = ['1299 S Junior Street', 'Philadelphia', 'Penysilvania', '19147'];
+const [street,city,state,zip='00000'] = address;
+console.log(`You are in ${street},${city},${state},${zip}`);
