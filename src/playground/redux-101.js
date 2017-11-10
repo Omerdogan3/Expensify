928584aae7828134b83c7805d125e6888784c927
Omer Dogan <omerdogan3@gmail.com>
2017-11-10 20:43:58 +0300

    refactor

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,16 +1,34 @@
 import { createStore } from 'redux';
 
+//Action Generators
+const incrementCount = ({incrementBy = 1} = {}) => ({
+    type:'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({decrementBy = 1} = {}) =>({
+    type:'DECREMENT',
+    decrementBy
+});
+
+const resetCount = () =>({
+    type:'RESET'
+});
+
+const setCount = ({count}) => ({
+    type:'SET',
+    count
+});
+
 const store = createStore((state = {count: 0}, action) => {
     switch(action.type){
-        case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy:1; 
+        case 'INCREMENT': 
             return{
-                count:state.count+incrementBy
+                count:state.count+ action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy:1;
             return{
-                count:state.count-decrementBy
+                count:state.count-action.decrementBy
             };
         case 'RESET':
             return{
@@ -26,33 +44,41 @@ const store = createStore((state = {count: 0}, action) => {
     }
 });
 
-
 const unsubscribe = store.subscribe(()=>{
     console.log(store.getState());
 });
 
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy:5
-});
+store.dispatch(incrementCount({incrementBy:5}));
+
+store.dispatch(incrementCount());
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({decrementBy:5}));
+
+store.dispatch(resetCount());
+
+store.dispatch(setCount({count:101}));
+
+
 
 // unsubscribe();
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy:10
-});
+// store.dispatch({
+//     type: 'DECREMENT',
+//     decrementBy:10
+// });
 
-store.dispatch({
-    type: 'RESET'
-});
+// store.dispatch({
+//     type: 'RESET'
+// });
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy:10
-});
+// store.dispatch({
+//     type: 'DECREMENT',
+//     decrementBy:10
+// });
 
-store.dispatch({
-    type: 'SET',
-    count:101
-});
+// store.dispatch({
+//     type: 'SET',
+//     count:101
+// });
