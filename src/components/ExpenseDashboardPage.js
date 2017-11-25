c9c71ddfbb40d54d18cd4b5dc334c475784be08f
Omer Dogan <omerdogan3@gmail.com>
2017-11-25 16:05:57 +0300

    Input Filters

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboardPage = () => (
     <div>
-       <ExpenseList/>
+        <ExpenseListFilters/>
+        <ExpenseList/>
     </div>
 );
 
