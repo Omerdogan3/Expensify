2d09d1e767ac0687134fa39dddd1e5d082f5ba7a
Omer Dogan <omerdogan3@gmail.com>
2017-12-07 19:38:56 +0300

    Enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import {shallow} from 'enzyme';
+// import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 //react-test-renderer
 
 test('should render header correctly',() =>{
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header/>);
-    console.log(renderer.getRenderOutput());
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    // expect(wrapper.find('h1').length).toBe(1);
+    expect(wrapper).toMatchSnapshot();
+
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header/>);
+    // console.log(renderer.getRenderOutput());
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
