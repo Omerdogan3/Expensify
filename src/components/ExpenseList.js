d839c9290066752ac9634b5a771e2dd6aeaadcfd
Omer Dogan <omerdogan3@gmail.com>
2017-12-12 17:05:09 +0300

    Setup Formation Date and Amount

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
     <div>
         <h1>ExpenseList</h1>
         {props.expenses.map((expense) => {
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-// import toJSON from 'enzyme-to-json';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
-//react-test-renderer
 
-test('should render header correctly',() =>{
-    const wrapper = shallow(<Header />);
-    // expect(wrapper.find('h1').length).toBe(1);
-    expect(wrapper).toMatchSnapshot();
-
-    // const renderer = new ReactShallowRenderer();
-    // renderer.render(<Header/>);
-    // console.log(renderer.getRenderOutput());
-    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+test('should render Header correctly', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
 });
