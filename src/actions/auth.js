53885b9792cd9e88c4cc8d8299e0f06043937242
Omer Dogan <omerdogan3@gmail.com>
2018-01-11 13:30:05 +0300

    Logout button

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,3 +4,9 @@ export const startLogin = () =>{
         return firebase.auth().signInWithPopup(googleAuthProvider);
     };
 };
+
+export const startLogout = () =>{
+    return () => {
+        return firebase.auth().signOut(); 
+    };
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {startLogout} from '../actions/auth';
 
-const Header = () => (
+export const Header = ({startLogout}) => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>DashBoard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>     
         <NavLink to="/help" activeClassName="is-active">Help Page</NavLink>
+        <button onClick={startLogout}>Logout</button>
     </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) =>({
+    startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
