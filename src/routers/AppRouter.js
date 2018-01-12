ac0b528f169e28ccad23a583d47afd0ac1f4a4a9
Omer Dogan <omerdogan3@gmail.com>
2018-01-12 18:35:32 +0300

    Private Routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,36 +1,29 @@
 import React from 'react';
-import { Router, Route, Switch, Link, NavLink} from 'react-router-dom';
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import AdExpensePage from '../components/AdExpensePage';
+import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AdExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
-import Header from '../components/Header';
-import LoginPage from  '../components/LoginPage';
-
-//create 6 new files.
-//setup imports.
-//import into appRouter.
+import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
-
-const AppRouter = () =>(
-    <Router history = {history}>
-        <div>
-            <Header/> 
-            <Switch>
-                <Route path="/" component={LoginPage} exact={true}/>
-                <Route path="/dashboard" component={ExpenseDashboardPage}/>
-                <Route path="/create" component={AdExpensePage}/>
-                <Route path="/edit/:id" component={EditExpensePage}/>
-                <Route path="/help" component={HelpPage}/>
-                <Route component={NotFoundPage}/>
-            </Switch>
-        </div>
-    </Router>
+const AppRouter = () => (
+  <Router history={history}>
+    <div>
+      <Switch>
+        <Route path="/" component={LoginPage} exact={true} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default AppRouter;
-
